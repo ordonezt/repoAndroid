dd6d17ff1075d90431ade0e86e511e6e8a190f1d
ordonezt <tomasbautista.o@gmail.com>
2022-05-08 17:42:17 -0300

    Haciendo pruebas del drawer, no anda nada :(

--- Nerdypedia/app/src/main/java/com/utn/nerdypedia/activities/MainActivity.kt
+++ Nerdypedia/app/src/main/java/com/utn/nerdypedia/activities/MainActivity.kt
@@ -2,11 +2,50 @@ package com.utn.nerdypedia.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.utn.nerdypedia.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+
+        navController = Navigation.findNavController(this, R.id.fragmentContainerView3)
+        navigationView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.drawer_menu, menu)
+        return true
+    }
+
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
